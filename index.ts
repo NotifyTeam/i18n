8b9ed0743b80cfefb6754371b3608b7b4eca6b04
Thomas Lekanger <datagutt@lekanger.no>
2019-12-06 23:17:06 +0100

    Norwegian translation

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import en from './json/en.json';
 import pt from './json/pt.json';
+import no from './json/no.json';
 import it from './json/it.json';
 
 export default () => {
-  return { en, pt, it };
+  return { en, pt, it, no };
 };
